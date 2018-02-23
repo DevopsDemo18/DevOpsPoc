467c00e8096b89e1604d7f48414d6c27d2285d37
DevopsDemo18 <devops.demo@yahoo.com>
2018-02-23 19:44:03 +0530

    Initial Commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //view test
+            return View();
+        }
+
+        public ActionResult ShowPageDetails()
+        {
+            EmployeeBasicModel contextEmployee = new EmployeeBasicModel();
+            contextEmployee.EmployeeName = Context.Item["Name"];
+            contextEmployee.EmployeeSalary = Context.Item["Salary"];
+
+            return View("~/Views/Home/ShowPageDetails.cshtml", contextEmployee);
+        }
+    }
+}
---
+++ WebApplication1/Models/EmployeeBasicModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class EmployeeBasicModel
+    {
+        public string EmployeeName { get; set; }
+
+        public string EmployeeSalary { get; set; }
+    }
+}
